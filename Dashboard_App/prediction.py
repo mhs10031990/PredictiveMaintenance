9fd1a16c6b6b1c7943d880cc4580ccaa5da554a3
mohanpawar <mohanpawar>
2023-04-05 14:02:13 +0000

    auto save by knights-watch

--- Dashboard_App/prediction.py
+++ Dashboard_App/prediction.py
@@ -10,7 +10,9 @@ def binary_model(payload):
 #     url = 'http://svc-1a0c7b69-afcc-4146-9444-9f19d15dee59-6fcd675fdc:5001/predictivemaintenancebinaryclassification/074e09f2-dec7-4179-829f-05bdfdbc798a/score'
     url = "https://refract.fosfor.com/predictivemaintenancebinaryclassification/074e09f2-dec7-4179-829f-05bdfdbc798a/score"
     data={"payload" : str(payload)}
+    return payload
     response_json = requests.post(url, json=data, headers=headers)
+    
     #st.text_input("API Response: ",response_json.content)
     response = response_json.json()
     try:
