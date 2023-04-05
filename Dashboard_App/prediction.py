dbf7d18316d728e3e925fe0ef6fbf4cf5b65156e
mohanpawar <mohanpawar>
2023-04-05 13:09:59 +0000

    auto save by knights-watch

--- Dashboard_App/prediction.py
+++ Dashboard_App/prediction.py
@@ -7,7 +7,7 @@ import json
 def binary_model(payload):
     
     headers={"Content-type":"application/json"}
-    url = 'http://svc-1a0c7b69-afcc-4146-9444-9f19d15dee59-6fcd675fdc-tbm6j:5001/predictivemaintenancebinaryclassification/074e09f2-dec7-4179-829f-05bdfdbc798a/score'
+    url = 'http://svc-1a0c7b69-afcc-4146-9444-9f19d15dee59-6fcd675fdc:5001/predictivemaintenancebinaryclassification/074e09f2-dec7-4179-829f-05bdfdbc798a/score'
     data={"payload" : str(payload)}
     response_json = requests.post(url, json=data, headers=headers)
     #st.text_input("API Response: ",response_json.content)
@@ -20,7 +20,7 @@ def binary_model(payload):
 def multi_model(payload):
     
     headers={"Content-type":"application/json"} 
-    url = 'http://svc-33df36f1-5b37-4b8b-995f-93562b2df15f-5b5f8fb999-sr78j:5001/predictivemaintenancemulticlassification/b6f10f20-e5b1-42f6-a9e2-de6a2a60b069/score'
+    url = 'http://svc-33df36f1-5b37-4b8b-995f-93562b2df15f-5b5f8fb999:5001/predictivemaintenancemulticlassification/b6f10f20-e5b1-42f6-a9e2-de6a2a60b069/score'
     data={"payload":str(payload)}
     response_json = requests.post(url, json=data, headers=headers)
     #st.text_input("API Response: ",response_json.content)
