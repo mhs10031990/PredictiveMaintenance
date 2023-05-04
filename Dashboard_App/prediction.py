253b95b8f0ffa996dd49d8471ab6882ca7c41b0a
mohanpawar <mohan.pawar@lntinfotech.com>
2023-05-04 07:47:52 +0000

    auto save by knights-watch

--- Dashboard_App/prediction.py
+++ Dashboard_App/prediction.py
@@ -7,7 +7,7 @@ import json
 def binary_model(payload):
     
     headers={"Content-type":"application/json"}
-    url = 'http://svc-3ed43365-272d-48cf-856a-deb1af27ee27:5001/predictivemaintenancebinaryclassification/ec2b1439-42f2-4c69-94e7-e52dd57c3e6c/score'
+    url = 'http://svc-15f321b3-dfe3-4dbd-83eb-903aa51a847f:5001/predictivemaintenancebinaryclassification/ec2b1439-42f2-4c69-94e7-e52dd57c3e6c/score'
     data={"payload" : str(payload)}
     response_json = requests.post(url, json=data, headers=headers)
     
@@ -21,7 +21,7 @@ def binary_model(payload):
 def multi_model(payload):
     
     headers={"Content-type":"application/json"} 
-    url = 'http://svc-23cff8be-6a11-492a-a1d4-61c71e7b0bdc:5001/predictivemaintenancemulticlassification/f6da9f28-e4ea-4226-80fd-8a5e5685b733/score'
+    url = 'http://svc-15f321b3-dfe3-4dbd-83eb-903aa51a847f:5001/predictivemaintenancemulticlassification/f6da9f28-e4ea-4226-80fd-8a5e5685b733/score'
 #     url = 
 
     data={"payload":str(payload)}
